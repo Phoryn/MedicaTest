8aa3021d6b2caeedefe29044cfd01348740121ea
Phoryn <mateusz@duda.pro>
2020-09-04 10:33:37 +0200

    Add project files.

---
+++ MedicaTest/Models/CarPart.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MedicaTest.Models
+{
+    public class CarPart
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public DateTime Added { get; set; }
+        public DateTime Modified { get; set; }
+    }
+}
---
+++ MedicaTest/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MedicaTest.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
